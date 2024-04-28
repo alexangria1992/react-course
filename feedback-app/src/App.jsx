bda8a524bcc1eb25b14de13dc8a843ca99ff274a
Alexandre Angrignon <alex040892@gmail.com>
2024-04-28 18:52:43 +1200

    Global State - feedback Items

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/feedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
