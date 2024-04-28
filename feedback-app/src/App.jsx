621aa102137985b4236c4a14d9a1eeabaf1c852f
Alexandre Angrignon <alex040892@gmail.com>
2024-04-28 20:05:24 +1200

    events and prop drilling

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackData from "./data/feedbackData";
 import FeedbackList from "./components/FeedbackList";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    // console.log("app", id);
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
       {/* <button onClick={handleClick}>Click</button> */}
     </Card>
