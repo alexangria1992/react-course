9ee4ec5fae73fa8456630a62f330b06d04cd230e
Alexandre Angrignon <alex040892@gmail.com>
2024-04-29 11:52:34 +1200

    Form Input State

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import FeedbackData from "./data/feedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
-
+import FeedbackForm from "./components/FeedbackForm";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
@@ -16,6 +16,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
