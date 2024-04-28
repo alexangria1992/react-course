48424cfabaf6f7af329b3f65fa9e77ee6a28fb01
Alexandre Angrignon <alex040892@gmail.com>
2024-04-28 13:20:10 +1200

    intro to jsx

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <h1>Hello from the app component</h1>;
+  return (
+    <>
+      <h1>Hello from the app component</h1>
+      <h1>test</h1>
+    </>
+  );
 }
 
 export default App;
