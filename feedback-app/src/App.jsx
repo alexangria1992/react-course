d36b9d3dd2c3c650678f1ea0b6cc06c421183215
Alexandre Angrignon <alex040892@gmail.com>
2024-04-28 16:11:45 +1200

    components, props and prop types

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,35 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  const loading = false;
-  const showComments = false;
-
-  if (loading) return <h1>Loading...</h1>;
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && (
-        <div className="comments">
-          <h3>Comments {comments.length}</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>{comment.text}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
