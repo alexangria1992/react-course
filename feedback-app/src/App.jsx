f6cb29710ce62ccde889b2b917598f3465d7b2fe
Alexandre Angrignon <alex040892@gmail.com>
2024-04-28 13:29:55 +1200

    Dynamic values & lists in jsx

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 function App() {
+  const title = "Blog Post";
+  const body = "This is my blog post";
+
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ];
   return (
-    <>
-      <h1>Hello from the app component</h1>
-      <h1>test</h1>
-    </>
+    <div className="container">
+      <h1>{title}</h1>
+      <h1>{body}</h1>
+
+      <div className="comments">
+        <h3>Comments {comments.length}</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
