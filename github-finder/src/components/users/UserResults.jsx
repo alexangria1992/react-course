88c82edee88e50b3776ec38960ef4fc29fa7df7f
Alexandre Angrignon <alex040892@gmail.com>
2024-05-01 10:38:28 +1200

    display users

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -24,8 +25,8 @@ function UserResults() {
   if (!isLoading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user, index) => (
-          <h3 key={index}>{user.login}</h3>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
