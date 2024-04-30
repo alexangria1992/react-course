5cf017ecbd62b78d2642b9f69319a53ede971e15
Alexandre Angrignon <alex040892@gmail.com>
2024-05-01 11:21:00 +1200

    reducers & useReducer hook

--- github-finder/src/context/github/GithubContext.jsx
+++ github-finder/src/context/github/GithubContext.jsx
@@ -1,13 +1,20 @@
-import { createContext, useState } from "react";
+//IMPORTS
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
+//Context
 const GithubContext = createContext();
 
 const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -18,15 +25,17 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json();
     // console.log(data);
-    setUsers(data);
-    setLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
