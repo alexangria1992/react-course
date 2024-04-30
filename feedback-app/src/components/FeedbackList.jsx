62193d830a4af4e4600d99de0a4c3d252fe493c7
Alexandre Angrignon <alex040892@gmail.com>
2024-04-30 14:45:00 +1200

    Spinner Component

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -2,14 +2,17 @@ import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>;
   }
   console.log(feedback);
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
