f1ab9ecac76c7e3cc8a12ede5675713865197cd1
Alexandre Angrignon <alex040892@gmail.com>
2024-05-01 16:34:31 +1200

    Move getUser to Actions file

--- github-finder/src/context/github/GithubActions.jsx
+++ github-finder/src/context/github/GithubActions.jsx
@@ -16,3 +16,37 @@ export const searchUsers = async (text) => {
   // console.log(data);
   return items;
 };
+
+//Get search results
+export const getUser = async (login) => {
+  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+
+  if (response.status === 404) {
+    window.location = "/notfound";
+  } else {
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  }
+};
+
+//Get search results
+export const getUserRepos = async (login) => {
+  const params = new URLSearchParams({
+    sort: "created",
+    per_page: 10,
+  });
+  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+
+  const data = await response.json();
+  // console.log(data);
+  return data;
+};
--- github-finder/src/context/github/GithubContext.jsx
+++ github-finder/src/context/github/GithubContext.jsx
@@ -5,9 +5,6 @@ import githubReducer from "./GithubReducer";
 //Context
 const GithubContext = createContext();
 
-const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
-const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   //Reducer State
   const initialState = {
@@ -20,70 +17,11 @@ export const GithubProvider = ({ children }) => {
   //useReducer hook
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //Get search results
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
-      // console.log(data);
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  //Get search results
-  const getUserRepos = async (login) => {
-    setLoading();
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-    });
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    );
-
-    const data = await response.json();
-    // console.log(data);
-    dispatch({
-      type: "GET_REPOS",
-      payload: data,
-    });
-  };
-
-  //clear users from state
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
-  const setLoading = () =>
-    dispatch({
-      type: "SET_LOADING",
-    });
-
   return (
     <GithubContext.Provider
       value={{
         ...state,
         dispatch,
-
-        clearUsers,
-        getUser,
-
-        getUserRepos,
       }}
     >
       {children}
