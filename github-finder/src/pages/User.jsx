a3184fdcdd567891686eb53b85cbdfebb579cfdb
Alexandre Angrignon <alex040892@gmail.com>
2024-05-01 16:50:01 +1200

    Cleaning Up Our Actions & Axios

--- github-finder/src/pages/User.jsx
+++ github-finder/src/pages/User.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
-import { getUserRepos, getUser } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext);
 
@@ -14,16 +14,10 @@ function User() {
       type: "SET_LOADING",
     });
     const getUserData = async () => {
-      const userData = await getUser(params.login);
+      const userData = await getUserAndRepos(params.login);
       dispatch({
-        type: "GET_USER",
+        type: "GET_USER_AND_REPOS",
         payload: userData,
-      });
-
-      const userRepoData = await getUserRepos(params.login);
-      dispatch({
-        type: "GET_REPOS",
-        payload: userRepoData,
       });
     };
     getUserData();
