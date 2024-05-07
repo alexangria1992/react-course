7098c525042b45e7eebb5754c7b827894943023a
Alexandre Angrignon <alex040892@gmail.com>
2024-05-07 17:02:16 +1200

    Profile Listings Delete

--- house-marketplace/src/components/ListingItem.jsx
+++ house-marketplace/src/components/ListingItem.jsx
@@ -1,45 +1,45 @@
-import { Link } from "react-router-dom";
-import bedIcon from "../assets/svg/bedIcon.svg";
-import bathtubIcon from "../assets/svg/bathtubIcon.svg";
+import { Link } from 'react-router-dom';
+import bedIcon from '../assets/svg/bedIcon.svg';
+import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 
 function ListingItem({ listing, id, onDelete }) {
   return (
-    <li className="categoryListing">
+    <li className='categoryListing'>
       <Link
         to={`/category/${listing.type}/${id}`}
-        className="categoryListingLink"
+        className='categoryListingLink'
       >
         <img
           src={listing.imgUrls[0]}
           alt={listing.name}
-          className="categoryListingImg"
+          className='categoryListingImg'
         />
-        <div className="categoryListingDetails">
-          <p className="categoryListingLocation">{listing.location}</p>
-          <p className="categoryListingName">{listing.name}</p>
-          <p className="categoryListingPrice">
+        <div className='categoryListingDetails'>
+          <p className='categoryListingLocation'>{listing.location}</p>
+          <p className='categoryListingName'>{listing.name}</p>
+          <p className='categoryListingPrice'>
             $
             {listing.offer
               ? listing.discountedPrice
                   .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
               : listing.regularPrice
                   .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-            {listing.type === "rent" && " / Month"}
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            {listing.type === 'rent' && ' / Month'}
           </p>
-          <div className="categoryListingInfoDiv">
-            <img src={bedIcon} alt="" />
-            <p className="categoryListingInfoText">
+          <div className='categoryListingInfoDiv'>
+            <img src={bedIcon} alt='' />
+            <p className='categoryListingInfoText'>
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} Bedrooms`
-                : "1 bedroom"}
+                : '1 bedroom'}
             </p>
-            <img src={bathtubIcon} alt="" />
-            <p className="categoryListingInfoText">
+            <img src={bathtubIcon} alt='' />
+            <p className='categoryListingInfoText'>
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} Bathrooms`
-                : "1 bathroom"}
+                : '1 bathroom'}
             </p>
           </div>
         </div>
@@ -47,14 +47,14 @@ function ListingItem({ listing, id, onDelete }) {
       {onDelete && (
         <svg
           onClick={() => onDelete(listing.id, listing.name)}
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 0 24 24"
-          width="24px"
-          fill="rgb(2311, 76"
+          xmlns='http://www.w3.org/2000/svg'
+          height='24px'
+          viewBox='0 0 24 24'
+          width='24px'
+          fill='rgb(231, 76, 60)'
         >
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+          <path d='M0 0h24v24H0z' fill='none' />
+          <path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z' />
         </svg>
       )}
     </li>
--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,13 +1,25 @@
-import { getAuth, updateProfile } from "firebase/auth";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { db } from "../firebase.config";
-import { updateDoc, doc } from "firebase/firestore";
-import { toast } from "react-toastify";
-import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
-import homeIcon from "../assets/svg/homeIcon.svg";
-
+import { getAuth, updateProfile } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { db } from '../firebase.config';
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore';
+import { toast } from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
+import ListingItem from '../components/ListingItem';
 function Profile() {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
   const auth = getAuth();
   const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
@@ -21,8 +33,36 @@ function Profile() {
 
   const onLogout = () => {
     auth.signOut();
-    navigate("/");
+    navigate('/');
   };
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, 'listings');
+
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      );
+
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
   const onSubmit = async () => {
     try {
@@ -33,14 +73,14 @@ function Profile() {
         });
 
         //update in firestore
-        const userRef = doc(db, "users", auth.currentUser.uid);
+        const userRef = doc(db, 'users', auth.currentUser.uid);
         await updateDoc(userRef, {
           name,
         });
       }
     } catch (error) {
       console.log(error);
-      toast.error("Could not update profile details");
+      toast.error('Could not update profile details');
     }
   };
 
@@ -50,53 +90,81 @@ function Profile() {
       [e.target.id]: e.target.value,
     }));
   };
+
+  const onDelete = async (listingId) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingId));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingId
+      );
+      setListings(updatedListings);
+      toast.success('Successfully deleted listing');
+    }
+  };
+
   return (
-    <div className="profile">
-      <header className="profileHeader">
-        <p className="pageHeader">My Profile</p>
-        <button className="logOut" onClick={onLogout}>
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button className='logOut' onClick={onLogout}>
           Logout
         </button>
       </header>
 
       <main>
-        <div className="profileDetailsHeader">
-          <p className="profileDetailsText">Personal Details</p>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
           <p
-            className="changePersonalDetails"
+            className='changePersonalDetails'
             onClick={() => {
               changeDetails && onSubmit();
               setChangeDetails((prevState) => !prevState);
             }}
           >
-            {changeDetails ? "done" : "change"}
+            {changeDetails ? 'done' : 'change'}
           </p>
         </div>
-        <div className="profileCard">
+        <div className='profileCard'>
           <form>
             <input
-              type="text"
-              id="name"
-              className={!changeDetails ? "profileName" : "profileNameActive"}
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
               disabled={!changeDetails}
               value={name}
               onChange={onChange}
             />
             <input
-              type="email"
-              id="email"
-              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              type='email'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
               disabled={!changeDetails}
               value={email}
               onChange={onChange}
             />
           </form>
         </div>
-        <Link to="/create-listing" className="createListing">
-          <img src={homeIcon} alt="" />
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='' />
           <p>Sell or rent your home</p>
-          <img src={arrowRight} alt="" />
+          <img src={arrowRight} alt='' />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className='listingText'>Your Listings</p>
+            <ul className='listingsList'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   );
